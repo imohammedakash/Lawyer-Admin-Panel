c8ad99a9d9f36e3ce1c054c1d49ef52093dafd4a
imohammedakash <mdakash2003evergreen@gmail.com>
2023-11-17 23:26:41 +0530

    Updated with Geo Location

--- src/Realtime.jsx
+++ src/Realtime.jsx
@@ -3,12 +3,14 @@ import React, { useState, useEffect } from 'react';
 const RealTimeLocation = () => {
   const [location, setLocation] = useState(null);
   const [error, setError] = useState(null);
+  const [changeCount, setChangeCount] = useState(0);
 
   useEffect(() => {
     const watchLocation = navigator.geolocation.watchPosition(
       (position) => {
         const { latitude, longitude } = position.coords;
         setLocation({ latitude, longitude });
+        setChangeCount((prevCount) => prevCount + 1);
       },
       (error) => {
         console.error('Error getting geolocation:', error.message);
@@ -24,7 +26,10 @@ const RealTimeLocation = () => {
     <div>
       <h1>Real-Time Location</h1>
       {location ? (
-        <p>Latitude: {location.latitude}, Longitude: {location.longitude}</p>
+        <>
+          <p>Latitude: {location.latitude}, Longitude: {location.longitude}</p>
+          <p>Coordinate Changes: {changeCount}</p>
+        </>
       ) : (
         <p>Loading location...</p>
       )}
